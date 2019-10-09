bdcf0f58eabf52a85f24a12c6c02ab044e9f8426
aTan <atan.aka.xellos@gmail.com>
2019-10-09 23:55:22 +0300

    update binary search with 1 comparison

--- src/homework1/BinarySearch.java
+++ src/homework1/BinarySearch.java
@@ -3,55 +3,64 @@ package homework1;
 public class BinarySearch {
 
     private static int binarySearch(int[] a, int key) {
-        int lo = 0, hi = a.length - 1, mid;
+        if (a.length == 0) return -1;
 
-        while (lo <= hi) {
+        int lo = 0, hi = a.length, mid;
+
+        while (hi - lo > 1) {
             mid = (lo + hi) >>> 1;
 
-            if (a[mid] > key)
-                hi = mid - 1;
-            else if (a[mid] < key)
-                lo = mid + 1;
-            else return mid;
+            if (a[mid] <= key)
+                lo = mid;
+            else {
+                hi = mid;
+            }
         }
+
+        if (a[lo] == key) return lo;
         return -1;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(binarySearch(new int[]{}, 0) == -1);
-        System.out.println(binarySearch(new int[]{0}, 0)== 0);
-        System.out.println(binarySearch(new int[]{0}, 1)== -1);
+        int a = Integer.MAX_VALUE - 100;
+        int b = ((Integer.MAX_VALUE - 100) + (Integer.MAX_VALUE - 100)) >>> 1;
+        System.out.println(a == b);
+
+        System.out.println(binarySearch(new int[]{}, 0)  == -1);
+        System.out.println(binarySearch(new int[]{0}, 0) == 0);
+        System.out.println(binarySearch(new int[]{0}, 1) == -1);
 
         System.out.println(binarySearch(new int[]{0, 1}, 0) == 0);
         System.out.println(binarySearch(new int[]{0, 1}, 1) == 1);
-        System.out.println(binarySearch(new int[]{0, 1}, 2)== -1);
+        System.out.println(binarySearch(new int[]{0, 1}, 2) == -1);
 
         System.out.println(binarySearch(new int[]{0, 1, 2}, 0) == 0);
         System.out.println(binarySearch(new int[]{0, 1, 2}, 1) == 1);
         System.out.println(binarySearch(new int[]{0, 1, 2}, 2) == 2);
-        System.out.println(binarySearch(new int[]{0, 1, 2}, 3)== -1);
+        System.out.println(binarySearch(new int[]{0, 1, 2}, 3) == -1);
 
         System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 0) == 0);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 1) == 1);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 2) == 2);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 3) == 3);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 4)== -1);
+        System.out.println(binarySearch(new int[]{0, 1, 2, 3}, 4) == -1);
 
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 0) == 0);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 1) == 1);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 2) == 2);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 3) == 3);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 4) == 4);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 5)== -1);
+        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4}, 5) == -1);
 
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 0) == 0);
         System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 1) == 1);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 2) == 2);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 3) == 3);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 4) == 4);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 5) == 5);
-        System.out.println(binarySearch(new int[]{0, 1, 2, 3, 4, 5}, 6) == -1);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 3) == 2);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 5) == 3);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 8) == 4);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 9) == 5);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 2) == -1);
+        System.out.println(binarySearch(new int[]{0, 1, 3, 5, 8, 9}, 7) == -1);
     }
 
 
