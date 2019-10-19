b3ff0219d8107cf403e71327e1191cd82a2f5867
aTan <atan.aka.xellos@gmail.com>
2019-10-20 01:18:42 +0300

    update insertion sort analysis, still with bug

--- src/homework3/InsertionSortAnalysis.java
+++ src/homework3/InsertionSortAnalysis.java
@@ -4,23 +4,18 @@ public class InsertionSortAnalysis {
 
     public static void main(String[] args) {
 
-        int result;
+//        System.out.println(insertionSort(new int[]{4,3,2,1}) == 6);
+//        System.out.println(insertionSort(new int[]{5,4,3,2,1}) == 10);
+//        System.out.println(insertionSort(new int[]{2,1,3,1,2}) == 4);
+//        System.out.println(insertionSort(new int[]{1,1,2,2,2}) == 0);
 
-//        result = insertionSort(new int[]{4,3,2,1}); // 6
-//        System.out.println(result);
-
-        result = insertionSort(new int[]{2, 1, 3, 1, 2}); // 4
-        System.out.println(result);
-
-//        result = insertionSort(new int[]{1, 1, 2, 2, 2}); // 0
-//        System.out.println(result);
-
+        System.out.println(insertionSort(new int[]{12,15,1,5,6,14,11}) == 10);
     }
 
     private static int[] heap;
     private static int heapSize;
 
-    static int insertionSort(int[] arr) {
+    private static int insertionSort(int[] arr) {
         heapSize = 0;
         heap = new int[arr.length + 1];
         int count = 0;
@@ -31,31 +26,47 @@ public class InsertionSortAnalysis {
         return count;
     }
 
-    // return size of sub-tree (heap.length - final_position)
+    // return size of sub-tree
     private static int insert(int i) {
-        int count = 0;
-
         heapSize++;
-        int index = heapSize;
         heap[heapSize] = i;
-
+        int index = heapSize;
 
         while (index > 1 && heap[index] > heap[index / 2]) {
             swap(index, index / 2);
-            index = index >> 1;
-            count++;
+            index >>= 1;
         }
-
-
-        return count;
+        return countNodes(index) - 1;
     }
 
+    private static int countNodes(int position) {
+        int sum = 0;
+        if (position > heapSize) return 0;
 
+        sum += countNodes(2 * position);
+        sum += countNodes(2 * position + 1);
+
+        return sum + 1;
+    }
 
     private static void swap(int i, int j) {
         int temp = heap[j];
         heap[j] = heap[i];
         heap[i] = temp;
     }
+
+//        heap = new int[]{-1, 10,9,8,7,6,5,4,3,2,1};
+//        heapSize = 10;
+//
+//        System.out.println(countNodes(1) - 1); // 9
+//        System.out.println(countNodes(2) - 1); // 5
+//        System.out.println(countNodes(3) - 1); // 2
+//        System.out.println(countNodes(4) - 1); // 2
+//        System.out.println(countNodes(5) - 1); // 1
+//        System.out.println(countNodes(6) - 1); // 0
+//        System.out.println(countNodes(7) - 1); // 0
+//        System.out.println(countNodes(8) - 1); // 0
+//        System.out.println(countNodes(9) - 1); // 0
+//        System.out.println(countNodes(10) - 1); // 0
 
 }
