363a6564c756149dc5b08b681c7c0509d5184804
aTan <atan.aka.xellos@gmail.com>
2019-10-22 19:32:59 +0300

    try to use BIT for InsertionSortAnalysis with fail

--- src/homework3/InsertionSortAnalysis.java
+++ src/homework3/InsertionSortAnalysis.java
@@ -4,69 +4,68 @@ public class InsertionSortAnalysis {
 
     public static void main(String[] args) {
 
-//        System.out.println(insertionSort(new int[]{4,3,2,1}) == 6);
-//        System.out.println(insertionSort(new int[]{5,4,3,2,1}) == 10);
-//        System.out.println(insertionSort(new int[]{2,1,3,1,2}) == 4);
-//        System.out.println(insertionSort(new int[]{1,1,2,2,2}) == 0);
-
+        System.out.println(insertionSort(new int[]{4,3,2,1}) == 6);
+        System.out.println(insertionSort(new int[]{5,4,3,2,1}) == 10);
+        System.out.println(insertionSort(new int[]{2,1,3,1,2}) == 4);
+        System.out.println(insertionSort(new int[]{1,1,2,2,2}) == 0);
         System.out.println(insertionSort(new int[]{12,15,1,5,6,14,11}) == 10);
     }
 
-    private static int[] heap;
-    private static int heapSize;
+    /**
+     * Count total number of elements already in the BIT, less than the one being inserted now.
+     * With current impl use 38Mb+ memory and won't work for values of high range.
+     *
+     * https://www.topcoder.com/community/competitive-programming/tutorials/binary-indexed-trees
+     * https://www.youtube.com/watch?v=CWDQJGaN1gY
+     * https://www.youtube.com/watch?v=v_wj_mOAlig
+     */
+    private static long insertionSort(int[] arr) {
+        long count = 0;
 
-    private static int insertionSort(int[] arr) {
-        heapSize = 0;
-        heap = new int[arr.length + 1];
-        int count = 0;
+        // this is just stupid, how to get rid of it?
+        int wtf_38_dot_1469_mb_size = 10_000_000;
+
+        BinaryIndexedTree bit = new BinaryIndexedTree(wtf_38_dot_1469_mb_size);
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            count += insert(arr[i]);
+            bit.update(arr[i]);
+            count += bit.read(arr[i] - 1);
         }
+
         return count;
     }
 
-    // return size of sub-tree
-    private static int insert(int i) {
-        heapSize++;
-        heap[heapSize] = i;
-        int index = heapSize;
+    /**
+     * In binary notation num can be represented as a1b, where a represents binary digits before the last bit and b represents zeroes after the last bit.
+     * Integer -num is equal to (a1b)¯ + 1 = a¯0b¯ + 1. b consists of all zeroes, so b¯ consists of all ones.
+     * -num = (a1b)¯ + 1 = a¯0b¯ + 1 = a¯1b.
+     * To isolate the last bit of num, need to use bitwise operator AND between num and -num:
+     * a1b & a¯1b
+     * (c)  https://www.topcoder.com/community/competitive-programming/tutorials/binary-indexed-trees
+     */
+    static class BinaryIndexedTree {
 
-        while (index > 1 && heap[index] > heap[index / 2]) {
-            swap(index, index / 2);
-            index >>= 1;
+        int[] tree;
+        BinaryIndexedTree(int size) {
+            tree = new int[size];
         }
-        return countNodes(index) - 1;
+
+        void update(int index) {
+            while (index < tree.length) {
+                tree[index]++;
+                // extracting least significant non-zero bit
+                index += (index & -index);
+            }
+        }
+
+        long read(int index) {
+            long sum = 0;
+            while (index > 0) {
+                sum += tree[index];
+                index -= (index & -index);
+            }
+            return sum;
+        }
     }
-
-    private static int countNodes(int position) {
-        int sum = 0;
-        if (position > heapSize) return 0;
-
-        sum += countNodes(2 * position);
-        sum += countNodes(2 * position + 1);
-
-        return sum + 1;
-    }
-
-    private static void swap(int i, int j) {
-        int temp = heap[j];
-        heap[j] = heap[i];
-        heap[i] = temp;
-    }
-
-//        heap = new int[]{-1, 10,9,8,7,6,5,4,3,2,1};
-//        heapSize = 10;
-//
-//        System.out.println(countNodes(1) - 1); // 9
-//        System.out.println(countNodes(2) - 1); // 5
-//        System.out.println(countNodes(3) - 1); // 2
-//        System.out.println(countNodes(4) - 1); // 2
-//        System.out.println(countNodes(5) - 1); // 1
-//        System.out.println(countNodes(6) - 1); // 0
-//        System.out.println(countNodes(7) - 1); // 0
-//        System.out.println(countNodes(8) - 1); // 0
-//        System.out.println(countNodes(9) - 1); // 0
-//        System.out.println(countNodes(10) - 1); // 0
-
+    
 }
