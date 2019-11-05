1aedf474a0d22b63d5c2f448f8f33815d5f8f6c1
aTan <atan.aka.xellos@gmail.com>
2019-11-05 17:47:58 +0200

    add templates for homework5

---
+++ src/homework5/PlayGame.java
@@ -0,0 +1,75 @@
+package homework5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PlayGame {
+
+    private static PrintWriter out = new PrintWriter(System.out);
+    private static InputReader in = new InputReader(System.in);
+
+    public static void main(String[] args) {
+
+        int n = in.nextInt();
+
+
+        out.println();
+
+        out.flush();
+        out.close();
+    }
+
+    private static void solve() {
+
+    }
+
+    private static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        boolean hasNext() {
+            try {
+                return reader.ready();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+    }
+}
